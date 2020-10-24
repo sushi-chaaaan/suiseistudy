d2f5e269fba8a75dc27cf705aec6dd7bee68f541
sushi-chaaaan <sushi_code@outlook.jp>
2020-10-24 22:59:36 +0900

    bot detect

--- discordbot.py
+++ discordbot.py
@@ -12,7 +12,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == client.user or message.author ==discord.User.bot:
         return
 
     if message.content.startswith('ﾎｼﾏﾁｰ!!'):
