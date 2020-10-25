fa83ce9f7f227ae7be33f2685f410ca79b74d94c
sushi-chaaaan <sushi_code@outlook.jp>
2020-10-25 09:37:00 +0900

    vc notify

--- discordbot.py
+++ discordbot.py
@@ -3,13 +3,16 @@ import os
 import traceback
 from datetime import datetime, timedelta
 
+#token
 token = os.environ['DISCORD_BOT_TOKEN']
+
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -26,10 +29,10 @@ async def on_voice_state_update(member,before,after) :
         now = datetime.utcnow() + timedelta(hours=9)
         alert_channel = client.get_channel(769718981908758589)
         if before.channel is None: 
-            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
+            msg = f'{now:%m/%d-%H:%M:%S} に {member.name} が {after.channel.name} に参加しました。'
             await alert_channel.send(msg)
         elif after.channel is None: 
-            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
+            msg = f'{now:%m/%d-%H:%M:%S} に {member.name} が {before.channel.name} から退出しました。'
             await alert_channel.send(msg)
 
 
